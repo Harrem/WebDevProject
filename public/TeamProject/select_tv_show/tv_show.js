5ec70faa648b338ba2a58fce032ad59be4e4b2c1
Harrem <52822616+Harrem@users.noreply.github.com>
2022-04-24 23:14:31 +0300

    commit
    select tv show and movies

--- public/TeamProject/select_tv_show/tv_show.js
+++ public/TeamProject/select_tv_show/tv_show.js
@@ -12,70 +12,163 @@ window.onscroll = function () {
   CheckNav();
 };
 //Navigation
-var nav = document.getElementById("navigation");
+var nav = document.getElementById("nav");
 function CheckNav() {
   var range = window.scrollY * 0.0025;
   nav.style.backgroundColor = `rgb(20,20,20,${range})`;
+  // nav.style.backgroundImage = `linear-gradient(to bottom, rgba(30, 30, 30,${range}), rgb(0, 0, 0, 0))`
 
   if (window.scrollY >= 400) {
     range = 255;
   }
 }
-
-window.onresize = function () {
-  if (window.innerWidth <= 1080) {
-    nav.style.fontSize = 0 + "px";
-    nav.style.scale = 0 + "px";
-    search.style.opacity = 0 + "%";
-    search.disabled = true;
-    v = true;
-    // search.style.width = 50 + "px";
-    // search.style.height = 50 + "px";
-    // search.style.marginRight = 40 + "px";
-    // search.style.borderRadius = 25 + "px";
-    logo.style.float = "none";
-    logo.style.marginLeft = window.innerWidth / 2 - 130 + "px";
-  } else if (window.innerWidth > 1080) {
-    nav.style.fontSize = 25 + "px";
-    nav.style.scale = 1 + "px";
-    // search.disabled = false;
-    // search.style.width = 250 + "px";
-    // search.style.height = 35 + "px";
-    // search.style.marginRight = 100 + "px";
-    // search.style.borderRadius = 30 + "px";
-    search.style.opacity = 100 + "%";
-    logo.style.float = "left";
-    logo.style.marginLeft = 0 + "px";
+//topPopelarMovie
+fetch(
+  "https://api.themoviedb.org/3/movie/popular?api_key=717eacf2852518ed1f0a438d848f9334",
+  {
+    method: "GET",
   }
-};
-
-fetch("https://imdb-api.com/en/API/ComingSoon/k_py4ssa7a", {
-  method: "GET",
-})
+)
   .then((response) => response.json())
-  .then((data) => {
+  .then(async (data) => {
     console.log(data);
-    const list = data.items;
+    const list = data.results;
 
     list.map((item) => {
-      const title = item.fullTitle;
-      const year = item.year;
-      const score = item.imDbRating;
-      const img = item.image;
-      const movie =
-        '<div class="movie"><img src=' +
-        img +
-        ' alt="Lamp"><h2>' +
-        title +
-        "</h2><h2>" +
-        year +
-        "</h2><h2>" +
-        score +
-        "</h2></div>";
-      document.getElementById("movies").innerHTML += movie;
+      const id = item.id;
+      const title = item.title;
+      const score = item.vote_average;
+      const poster = "http://image.tmdb.org/t/p/w500/" + item.poster_path;
+      // const backdrop = "http://image.tmdb.org/t/p/w1280/" + item.backdrop_path;
+      const year = item.release_date;
+
+      const movie = `<a href="../select_movies/movie.html">
+      <div class="movie">
+          <img class="posters" src="${poster}" alt="Poster">
+          <div id="textContainer">
+            <h2 style="color: black" id="title">${title}</h2>
+            <h4 style="color: black">${year}</h4>
+            <h4 style="color: black">${score}</h4>
+          </div>
+        </div></a>`;
+      document.getElementById("mostPopularMovie").innerHTML += movie;
     });
   })
 
   .catch((err) => {
     console.error(err);
   });
+
+//topPopelarTvShow
+fetch(
+  "https://api.themoviedb.org/3/tv/popular?api_key=717eacf2852518ed1f0a438d848f9334&page=1",
+  {
+    method: "GET",
+  }
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    const list = data.results;
+
+    list.map((item) => {
+      const id = item.id;
+      const title = item.name;
+      const score = item.vote_average;
+      const poster = "http://image.tmdb.org/t/p/w500/" + item.poster_path;
+      // const backdrop = "http://image.tmdb.org/t/p/w1280/" + item.backdrop_path;
+      const year = item.first_air_date;
+
+      const movie = `<a href="../select_movies/movie.html">
+      <div class="movie">
+          <img class="posters" src="${poster}" alt="Poster">
+          <div id="textContainer">
+            <h2 style="color: black" id="title">${title}</h2>
+            <h4 style="color: black">${year}</h4>
+            <h4 style="color: black">${score}</h4>
+          </div>
+        </div></a>`;
+      document.getElementById("mostPopularTvShow").innerHTML += movie;
+    });
+  })
+
+  .catch((err) => {
+    console.error(err);
+  });
+
+//latestMovie
+fetch(
+  "https://api.themoviedb.org/3/movie/upcoming?api_key=717eacf2852518ed1f0a438d848f9334",
+  {
+    method: "GET",
+  }
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    const list = data.results;
+
+    list.map((item) => {
+      const id = item.id;
+      const title = item.title;
+      const score = item.vote_average;
+      const poster = "http://image.tmdb.org/t/p/w500/" + item.poster_path;
+      // const backdrop = "http://image.tmdb.org/t/p/w1280/" + item.backdrop_path;
+      const year = item.release_date;
+
+      const movie = `<a href="../select_movies/movie.html">
+      <div class="movie">
+          <img class="posters" src="${poster}" alt="Poster">
+          <div id="textContainer">
+            <h2 style="color: black" id="title">${title}</h2>
+            <h4 style="color: black">${year}</h4>
+            <h4 style="color: black">${score}</h4>
+          </div>
+        </div></a>`;
+      document.getElementById("latestMovie").innerHTML += movie;
+    });
+  })
+
+  .catch((err) => {
+    console.error(err);
+  });
+
+//latestTvShow
+fetch(
+  "https://api.themoviedb.org/3/tv/airing_today?api_key=717eacf2852518ed1f0a438d848f9334&language=en-US&page=1",
+  {
+    method: "GET",
+  }
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    const list = data.results;
+
+    list.map((item) => {
+      const id = item.id;
+      const title = item.name;
+      const score = item.vote_average;
+      const poster = "http://image.tmdb.org/t/p/w500/" + item.poster_path;
+      // const backdrop = "http://image.tmdb.org/t/p/w1280/" + item.backdrop_path;
+      const year = item.first_air_date;
+
+      const movie = `<a href="../select_movies/movie.html">
+      <div class="movie">
+          <img class="posters" src="${poster}" alt="Poster">
+          <div id="textContainer">
+            <h2 style="color: black" id="title">${title}</h2>
+            <h4 style="color: black">${year}</h4>
+            <h4 style="color: black">${score}</h4>
+          </div>
+        </div></a>`;
+      document.getElementById("latestTvShow").innerHTML += movie;
+    });
+  })
+
+  .catch((err) => {
+    console.error(err);
+  });
+
+////////////////////////////////////////////////
+/// the Trailar for the Movies
